70a1a3dfeb25d3b364bfdaa9d2f6157e7e0307ca
tien <vuongtien6200@gmail.com>
2026-06-29 00:36:15 +0700

    fix app.jsx

--- nutriwallet_frontend/src/App.jsx
+++ nutriwallet_frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
 import { router } from "./routes/router";
 
 function App() {
   return (
-    <ThemeProvider>
+    <>
       <RouterProvider router={router} />
-    </ThemeProvider>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: { fontSize: "14px" },
+        }}
+      />
+    </>
   );
 }
 
